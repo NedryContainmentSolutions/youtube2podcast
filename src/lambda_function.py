b5588fde2d27bc10898758c4baeb3fb54ca88623
Greg Ford <greg.ford.2@googlemail.com>
2024-08-02 11:01:11 +0000

    Removed all thumbnail code. Added logic for Apple

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -20,30 +20,46 @@ CONTENT_PATH = os.getenv("CONTENT_PATH")
 PLAYLIST_URL = os.getenv("PLAYLIST_URL")
 WEBHOOK_TARGET = os.getenv("WEBHOOK_TARGET")
 AUDIO_EXTENSION = "m4a"
-MAX_FILES_TO_DOWNLOAD = int(os.getenv("MAX_FILES_TO_DOWNLOAD","2"))
+MAX_FILES_TO_DOWNLOAD = int(os.getenv("MAX_FILES_TO_DOWNLOAD", "2"))
 
 youtube_url_prefix = "https://www.youtube.com/watch?v="
 s3_bucket_url = f"https://{BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com/{CONTENT_PATH}"
 log_file_name = "videos_downloaded.txt"
 data_folder = "/tmp"
 # For yt-dlp's cache
-os.environ['XDG_CACHE_HOME'] = '/tmp/yt-dlp/cache'
+os.environ["XDG_CACHE_HOME"] = "/tmp/yt-dlp/cache"
 
 # Set up the details for the podcast feed
 podcast_title = "My YouTube Playlist"
 podcast_description = "A list of videos from my YouTube channel."
+podcast_author = "Me"
+podcast_email = "anon@example.com"
+podcast_explicit = "no"
+podcast_category = "Leisure"
 podcast_siteURL = ""
 podcast_image = "logo.png"  # Put this logo image into your S3 bucket
 podcast_GUID = ""
 podcast_header = '<?xml version="1.0" encoding="UTF-8"?>\n<rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:podcast="https://podcastindex.org/namespace/1.0" xmlns:atom="http://www.w3.org/2005/Atom">'
+
 channel_header = (
     f"<channel>\n<title>{podcast_title}</title>\n"
     f"<description>{podcast_description}</description>\n"
+    f"<itunes:author>{podcast_author}</itunes:author>\n"
+    f"<itunes:summary>{podcast_description}</itunes:summary>\n"
+    f"<itunes:explicit>{podcast_explicit}</itunes:explicit>\n"
+    f"<itunes:category text=\"{podcast_category}\"/>\n"
+    f"<itunes:image href=\"{s3_bucket_url}{podcast_image}\"/>\n"
+    f"<itunes:owner>\n"
+    f"<itunes:name>{podcast_author}</itunes:name>\n"
+    f"<itunes:email>{podcast_email}</itunes:email>\n"
+    f"</itunes:owner>\n"
     f"<image>\n<title>{podcast_title}</title>\n"
     f"<url>{s3_bucket_url}{podcast_image}</url>\n</image>\n"
 )
 
+
 output_rss_filename = "podcast.rss"
+
 
 def download_audio_from_yt_video(url, format_code="140"):
     logger.info("---- Downloading audio")
@@ -66,7 +82,10 @@ def download_audio_from_yt_video(url, format_code="140"):
             info_with_audio_extension = dict(info)
             info_with_audio_extension["ext"] = AUDIO_EXTENSION
             # Return filename with the correct extension
-            return ydl.prepare_filename(info_with_audio_extension), info_with_audio_extension["description"], info_with_audio_extension["thumbnail"]
+            return (
+                ydl.prepare_filename(info_with_audio_extension),
+                info_with_audio_extension["description"],
+            )
 
     except Exception as e:
         logger.error(f"An error occurred: {e}")
@@ -97,8 +116,7 @@ def get_video_title(url):
         video_title = info_dict.get("title", None)
     logger.info("---- Done")
     safe_title = video_title.replace("|", " ")
-    encoded_text = html.escape(safe_title)
-    return safe_title
+    return html.escape(safe_title)
 
 
 def append_to_file(filename, string):
@@ -169,8 +187,8 @@ def get_file_from_s3(filename):
         return False
 
 
-def write_rss_file(filename):
-    # Logfile format: YouTubeURL | Title | Time/Date | GUID | S3 URL | FileSizeBytes | thumbnail_url | description
+def generate_rss_file(filename):
+    # Logfile format: YouTubeURL | Title | Time/Date | GUID | S3 URL | FileSizeBytes | description
     with open(f"{data_folder}/{output_rss_filename}", "w") as output_file:
         output_file.write(podcast_header)
         output_file.write("\n")
@@ -185,15 +203,17 @@ def write_rss_file(filename):
                     words = line.split("|")
                     output_file.write("<item>\n")
                     output_file.write(f"<title>{words[1]}</title>\n")
-                    if(len(words) > 7):
-                        output_file.write(f"<description>{words[0]}\n{words[7]}</description>\n")
+                    if len(words) > 7:
+                        output_file.write(
+                            f"<description>{words[0]}\n{words[6]}</description>\n"
+                        )
                     else:
                         output_file.write(f"<description>{words[0]}</description>\n")
                     output_file.write(f"<guid>{words[3]}</guid>\n")
-                    output_file.write(f"<pubDate>{words[2]}</pubDate>\n")
-                    output_file.write(f'<enclosure url="{words[4]}" type="audio/mpeg" length="{words[5]}"/>\n')
-                    if(len(words) > 7):
-                        output_file.write(f"<itunes:image>{words[6]}</itunes:image>\n")
+                    output_file.write(f"<pubDate>{words[2].strip()}</pubDate>\n")
+                    output_file.write(
+                        f'<enclosure url="{words[4]}" type="audio/mpeg" length="{words[5]}"/>\n'
+                    )
                     output_file.write("</item>\n")
 
         output_file.write("</channel>\n")
@@ -201,7 +221,7 @@ def write_rss_file(filename):
         output_file.close()
 
 
-def process_videos():    
+def process_videos():
     logger.info("Get videos from playlist")
     playlist = get_playlist(PLAYLIST_URL)
 
@@ -219,10 +239,14 @@ def process_videos():
             new_videos = True
             logger.info(f"-- {this_video} - New video to process")
             episode_GUID = str(uuid.uuid4())
-            
+
             logger.info("--- Download audio and process")
-            filepath, description, thumbnail_url = download_audio_from_yt_video(this_video)
-            description = html.escape(description[:500].replace('|'," ").replace('\n', ' '))
+            filepath, description = download_audio_from_yt_video(
+                this_video
+            )
+            description = html.escape(
+                description[:500].replace("|", " ").replace("\n", " ")
+            )
 
             if not filepath:
                 logger.error(f"ERROR: failed to download {this_video}")
@@ -232,7 +256,7 @@ def process_videos():
                 if upload_audio_to_s3(filepath, episode_GUID):
                     s3_URL = f"{s3_bucket_url}{episode_GUID}.mp4a"
                     title = get_video_title(this_video)
-                    # Logfile format: YouTubeURL | Title | Time/Date | GUID | S3 URL | FileSizeBytes | thumbnail_url | description
+                    # Logfile format: YouTubeURL | Title | Time/Date | GUID | S3 URL | FileSizeBytes | description
                     log_entry = (
                         f"{this_video}|"
                         f"{title}|"
@@ -240,37 +264,34 @@ def process_videos():
                         f"{episode_GUID}|"
                         f"{s3_URL}|"
                         f"{str(file_size)}|"
-                        f"{thumbnail_url}|"
                         f"{description}|"
                     )
 
                     append_to_file(log_file_name, log_entry)
                     # Post to webhook using WEBHOOK_TARGET
                     if WEBHOOK_TARGET:
-                        requests.post(url=WEBHOOK_TARGET, json={"content": f"New clip saved: {title}"})
+                        requests.post(
+                            url=WEBHOOK_TARGET,
+                            json={"content": f"New clip saved: {title}"},
+                        )
 
                     # Updating the RSS every time - less efficient but safer in case lambda times out
                     logger.info("Regenerating RSS file")
-                    write_rss_file(log_file_name)
-                    if upload_file_to_s3(log_file_name) and upload_file_to_s3(output_rss_filename):
+                    generate_rss_file(log_file_name)
+                    if upload_file_to_s3(log_file_name) and upload_file_to_s3(
+                        output_rss_filename
+                    ):
                         logger.info("Updated RSS file uploaded to S3")
                     else:
                         logger.info("Error with RSS file upload to S3")
-                    
+
                     files_downloaded += 1
                     if files_downloaded >= MAX_FILES_TO_DOWNLOAD:
-                        logger.info(f"Stopping because MAX_FILES_TO_DOWNLOAD is {MAX_FILES_TO_DOWNLOAD}")
+                        logger.info(
+                            f"Stopping because MAX_FILES_TO_DOWNLOAD is {MAX_FILES_TO_DOWNLOAD}"
+                        )
                         break
 
-    # if new_videos:
-    #     logger.info("Regenerating RSS file")
-    #     write_rss_file(log_file_name)
-    #     if upload_file_to_s3(log_file_name) and upload_file_to_s3(output_rss_filename):
-    #         logger.info("Updated RSS file uploaded to S3")
-    #     else:
-    #         logger.info("Error with RSS file upload to S3")
-    # else:
-    #     logger.info("Nothing new so nothing to do")
     if files_downloaded > 0:
         logger.info(f"Done - downloaded {files_downloaded} files")
     else:
